a23521387b90ea038782db8c674ca073df332865
Michael Yagi <michaeltyagi@gmail.com>
2015-01-31 13:35:59 -0500

    Enabled tab swiping with navigation drawer

--- app/src/main/java/ca/michaelyagi/recipeapplication/DetailFragment.java
+++ app/src/main/java/ca/michaelyagi/recipeapplication/DetailFragment.java
@@ -4,72 +4,17 @@ package ca.michaelyagi.recipeapplication;
 // Display's a recipe. User can choose to Edit or Delete recipe
 /******************************************************************/
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.ProgressDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTabHost;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
-import android.text.Html;
-import android.util.AttributeSet;
-import android.util.Base64;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.util.TypedValue;
-import android.view.Display;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
-import android.widget.ShareActionProvider;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpDelete;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by Michael on 12/26/2014.
@@ -77,6 +22,7 @@ import java.util.List;
 public class DetailFragment extends Fragment {
 
     private FragmentTabHost mTabHost;
+    private ViewPager mPager;
 
     private int recipeId;
     private String recipeUser;
@@ -104,11 +50,47 @@ public class DetailFragment extends Fragment {
         args.putString("recipe_user",recipeUser);
         args.putBoolean("recipe_ispublished",recipeIsPublished);
 
+        mPager = (ViewPager) rootView.findViewById(R.id.pager);
+
         mTabHost = (FragmentTabHost)rootView.findViewById(android.R.id.tabhost);
         mTabHost.setup(getActivity(),getChildFragmentManager(), R.id.tab_content);
 
         mTabHost.addTab(mTabHost.newTabSpec("recipeFragmentTab").setIndicator("Recipe"),RecipeTabFragment.class, args);
         mTabHost.addTab(mTabHost.newTabSpec("recipeImagesFragmentTab").setIndicator("Gallery"), RecipeImagesTabFragment.class, args);
+
+        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+
+            }
+
+            @Override
+            public void onPageSelected(int pageNumber) {
+                mTabHost.setCurrentTab(pageNumber);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+
+        mTabHost.setOnTabChangedListener(new FragmentTabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                int pageNumber = 0;
+                if (tabId.equals("recipeFragmentTab")) {
+                    pageNumber = 0;
+                } else if (tabId.equals("recipeImagesFragmentTab")) {
+                    pageNumber = 1;
+                }
+
+                mPager.setCurrentItem(pageNumber);
+            }
+        });
+
+        mPager.setAdapter(new CustomerPagerAdapter(getChildFragmentManager()));
 
         return rootView;
     }
@@ -134,5 +116,39 @@ public class DetailFragment extends Fragment {
 
         return true;
     }
+
+    private class CustomerPagerAdapter extends FragmentPagerAdapter {
+
+        public CustomerPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Bundle args = new Bundle();
+            args.putInt("recipe_id", recipeId);
+            args.putString("recipe_user",recipeUser);
+            args.putBoolean("recipe_ispublished",recipeIsPublished);
+
+            if (position == 0) {
+                RecipeTabFragment recipeTabFragment = new RecipeTabFragment();
+                recipeTabFragment.setArguments(args);
+                return recipeTabFragment;
+            } else if (position == 1) {
+                RecipeImagesTabFragment recipeImagesTabFragment = new RecipeImagesTabFragment();
+                recipeImagesTabFragment.setArguments(args);
+                return recipeImagesTabFragment;
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
+        }
+
+    }
+
 
 }
